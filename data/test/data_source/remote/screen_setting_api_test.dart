719b7e203c3578f8acf6dc7d481a25a30daee402
Zendy <nguyenduc.051194@gmail.com>
2021-09-30 23:51:25 +0700

    Flutter clean architecture

---
+++ data/test/data_source/remote/screen_setting_api_test.dart
@@ -0,0 +1,15 @@
+// import 'package:data/src/data_source/remote/base/api_client.dart';
+// import 'package:dio/dio.dart';
+// import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // late Dio dio;
+  // late LogApiService log;
+  // late ApiClient client;
+  //
+  // setUp(() {
+  //   dio = Dio(BaseOptions(baseUrl: baseUrl));
+  //   log = LogApiService(baseUrl);
+  //   client = ApiClient(dio, log);
+  // });
+}
